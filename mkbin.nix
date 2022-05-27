c389db4423f0ad7ec4bb6179980c9ead23b9f9c3
Michael Bock <me@thought2.de>
2022-05-27 14:58:55 +0200

    Make bin

--- mkbin.nix
+++ mkbin.nix
@@ -16,16 +16,16 @@ let
         rev = "e00a54ca6bd0290e8301eff140d109c1f300e40d";
         sha256 = "1yrnnpxkzs59ik5dj9v67ysn4viff775v24kizpl0ylf24c74928";
       }
-    ) {
-    inherit pkgs;
-  };
+    )
+    {
+      inherit pkgs;
+    };
 
 in
 pkgs.mkShell {
   nativeBuildInputs = [
     easy-purescript-nix.purs-0_13_8
     easy-purescript-nix.spago
-    nodejs
   ];
   shellHook = ''
     npm install
